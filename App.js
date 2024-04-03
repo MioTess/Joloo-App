082d2738cfa7e4fc72750f4823775a19f1a7afa2
Bolormaa <bolormaabatnasan63>
2024-04-03 08:11:08 -0700

    Bolormaa

--- App.js
+++ App.js
@@ -6,12 +6,12 @@ import GroupTest from "./src/Screens/GroupTest";
 import GroupTestScreen from "./src/Screens/GroupTestScreen";
 
 const Stack = createNativeStackNavigator();
-// \hi
+//Bolormaa
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Горим сонгох">
-        <Stack.Screen name="Горим сонгох" component={HomeScreen} />
+        <Stack.Screen name="Горм сонгох" component={HomeScreen} />
         <Stack.Screen name="Бүлэг сонгох" component={GroupTestScreen} />
         <Stack.Screen name="Тест өгөх" component={GroupTest} />
       </Stack.Navigator>
